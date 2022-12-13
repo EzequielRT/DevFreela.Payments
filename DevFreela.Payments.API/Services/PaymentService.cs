2c2e6d28b7038e5b0946e99eb2adc04f31b91bd5
EzequielRT <ezefuts@gmail.com>
2022-12-13 18:18:11 -0300

    Add project files.

---
+++ DevFreela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,12 @@
+﻿using DevFreela.Payments.API.Models;
+
+namespace DevFreela.Payments.API.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public async Task<bool> Process(PaymentInfoInputModel paymentInfoInputModel)
+        {
+            return await Task.FromResult(true);
+        }
+    }
+}
